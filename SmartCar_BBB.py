73016399adac0aae82f6d969f25b28b52f779ab9
quanpower <quanpower@gmail.com>
2015-01-15 20:43:03 +0800

    edit SmartCar_BBB.py -add ultrasonic measuring

--- SmartCar_BBB.py
+++ SmartCar_BBB.py
@@ -5,7 +5,17 @@ __author__ = 'quanpower@gmail.com'
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-pins = ["P8_7","P8_8","P8_9","P8_10","P8_11","P8_12"]
+# SET PINS
+LeftMotorPin1 = "P8_7"
+LeftMotorPin2 = "P8_8"
+RightMotorPin1 = "P8_9"
+RightMotorPin2 = "P8_10"
+TrigPin = "P8_11"
+EchoPin = "P8_12"
+
+CurrDist = 0
+
+pins = [LeftMotorPin1,LeftMotorPin2,RightMotorPin1,RightMotorPin2,TrigPin]
 
 def setup():
     # 输出模式
@@ -13,46 +23,75 @@ def setup():
         GPIO.setup(pin, GPIO.OUT)
         GPIO.output(pin, GPIO.LOW)
         print "pin %s has been set to GPIO.OUT" % pin
+    # SET PIN_12 TO INPUT
+    GPIO.setup(EchoPin, GPIO.INPUT)
 
 def forward():
-    GPIO.output("P8_7", GPIO.HIGH)
-    GPIO.output("P8_8", GPIO.LOW)
-    GPIO.output("P8_9", GPIO.HIGH)
-    GPIO.output("P8_10", GPIO.LOW)
+    GPIO.output(LeftMotorPin1, GPIO.HIGH)
+    GPIO.output(LeftMotorPin2, GPIO.LOW)
+    GPIO.output(RightMotorPin1, GPIO.HIGH)
+    GPIO.output(RightMotorPin2, GPIO.LOW)
     print "forward"
 
 def backward():
-    GPIO.output("P8_7", GPIO.LOW)
-    GPIO.output("P8_8", GPIO.HIGH)
-    GPIO.output("P8_9", GPIO.LOW)
-    GPIO.output("P8_10", GPIO.HIGH)
+    GPIO.output(LeftMotorPin1, GPIO.LOW)
+    GPIO.output(LeftMotorPin2, GPIO.HIGH)
+    GPIO.output(RightMotorPin1, GPIO.LOW)
+    GPIO.output(RightMotorPin2, GPIO.HIGH)
     print "backward"
     time.sleep(1)
 
 def halt():
-    GPIO.output("P8_7", GPIO.HIGH)
-    GPIO.output("P8_8", GPIO.HIGH)
-    GPIO.output("P8_9", GPIO.HIGH)
-    GPIO.output("P8_10", GPIO.HIGH)
+    GPIO.output(LeftMotorPin1, GPIO.HIGH)
+    GPIO.output(LeftMotorPin2, GPIO.HIGH)
+    GPIO.output(RightMotorPin1, GPIO.HIGH)
+    GPIO.output(RightMotorPin2, GPIO.HIGH)
     time.sleep(0.5)
 
 def turn_left():
-    GPIO.output("P8_7", GPIO.LOW)
-    GPIO.output("P8_8", GPIO.HIGH)
-    GPIO.output("P8_9", GPIO.HIGH)
-    GPIO.output("P8_10", GPIO.LOW)
+    GPIO.output(LeftMotorPin1, GPIO.LOW)
+    GPIO.output(LeftMotorPin2, GPIO.HIGH)
+    GPIO.output(RightMotorPin1, GPIO.HIGH)
+    GPIO.output(RightMotorPin2, GPIO.LOW)
     print "turn left"
     time.sleep(1)
     halt()
 
 def turn_right():
-    GPIO.output("P8_7", GPIO.HIGH)
-    GPIO.output("P8_8", GPIO.LOW)
-    GPIO.output("P8_9", GPIO.LOW)
-    GPIO.output("P8_10", GPIO.HIGH)
+    GPIO.output(LeftMotorPin1, GPIO.HIGH)
+    GPIO.output(LeftMotorPin2, GPIO.LOW)
+    GPIO.output(RightMotorPin1, GPIO.LOW)
+    GPIO.output(RightMotorPin2, GPIO.HIGH)
     print "turn right"
     time.sleep(1)
     halt()
+
+def measuring_distance():
+    GPIO.output(TrigPin, GPIO.LOW)
+    time.sleep(0.000002)
+
+    GPIO.output(TrigPin, GPIO.HIGH)
+    time.sleep(0.000015)
+    GPIO.output(TrigPin, GPIO.LOW)
+
+    while GPIO.input(EchoPin)==0:
+        start = time.time()
+
+    while GPIO.input(EchoPin)==1:
+        stop = time.time()
+
+    # Calculate pulse length
+    elapsed = stop-start
+
+    # Distance pulse travelled in that time is time
+    # multiplied by the speed of sound (cm/s)
+    distance = elapsed * 34300
+
+    # That was the distance there and back so halve the value
+    distance = distance / 2
+
+    print "Distance : %.1f" % distance
+
 
 def loop():
     while True:
@@ -64,6 +103,7 @@ def loop():
         time.sleep(1)
         turn_right()
         time.sleep(1)
+        measuring_distance()
 
 def destroy():
     GPIO.cleanup()
